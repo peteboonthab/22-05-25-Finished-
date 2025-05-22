e0f99ab030ea100c2badc290e43776def0869e83
peteboonthab <0287763@schoolsnet.act.edu.au>
2025-05-23 00:33:24 +1000

    Add files via upload

---
+++ Form5.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Original1
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Right3Button = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.CorrectButton1 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.CorrectButton = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblCheckResult1 = new System.Windows.Forms.Label();
+            this.lblCheckResult2 = new System.Windows.Forms.Label();
+            this.lblCheckResult3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Back to Lesson";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Back5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(337, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Excellent";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.WrongBtn3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(458, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Try again";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.WrongBtn3_Click);
+            // 
+            // Right3Button
+            // 
+            this.Right3Button.Location = new System.Drawing.Point(581, 335);
+            this.Right3Button.Name = "Right3Button";
+            this.Right3Button.Size = new System.Drawing.Size(75, 23);
+            this.Right3Button.TabIndex = 4;
+            this.Right3Button.Text = "Good job";
+            this.Right3Button.UseVisualStyleBackColor = true;
+            this.Right3Button.Click += new System.EventHandler(this.CorrectBtn3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(337, 118);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "10";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.WrongBtn1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(419, 118);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "\"Input\"";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.WrongBtn1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(500, 118);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Output";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.WrongBtn1_Click);
+            // 
+            // CorrectButton1
+            // 
+            this.CorrectButton1.Location = new System.Drawing.Point(581, 118);
+            this.CorrectButton1.Name = "CorrectButton1";
+            this.CorrectButton1.Size = new System.Drawing.Size(75, 23);
+            this.CorrectButton1.TabIndex = 8;
+            this.CorrectButton1.Text = "Input";
+            this.CorrectButton1.UseVisualStyleBackColor = true;
+            this.CorrectButton1.Click += new System.EventHandler(this.CorrectBtn1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(419, 161);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "text";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(337, 161);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(75, 23);
+            this.RightButton.TabIndex = 10;
+            this.RightButton.Text = "int";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.CorrectBtn2_Click);
+            // 
+            // CorrectButton
+            // 
+            this.CorrectButton.Location = new System.Drawing.Point(581, 161);
+            this.CorrectButton.Name = "CorrectButton";
+            this.CorrectButton.Size = new System.Drawing.Size(75, 23);
+            this.CorrectButton.TabIndex = 11;
+            this.CorrectButton.Text = "string";
+            this.CorrectButton.UseVisualStyleBackColor = true;
+            this.CorrectButton.Click += new System.EventHandler(this.CorrectButton_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(500, 161);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "input";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(308, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(308, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 205);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(308, 212);
+            this.textBox3.TabIndex = 15;
+            // 
+            // lblCheckResult1
+            // 
+            this.lblCheckResult1.AutoSize = true;
+            this.lblCheckResult1.Location = new System.Drawing.Point(690, 125);
+            this.lblCheckResult1.Name = "lblCheckResult1";
+            this.lblCheckResult1.Size = new System.Drawing.Size(41, 13);
+            this.lblCheckResult1.TabIndex = 16;
+            this.lblCheckResult1.Text = "Check!";
+            // 
+            // lblCheckResult2
+            // 
+            this.lblCheckResult2.AutoSize = true;
+            this.lblCheckResult2.Location = new System.Drawing.Point(690, 164);
+            this.lblCheckResult2.Name = "lblCheckResult2";
+            this.lblCheckResult2.Size = new System.Drawing.Size(41, 13);
+            this.lblCheckResult2.TabIndex = 17;
+            this.lblCheckResult2.Text = "Check!";
+            // 
+            // lblCheckResult3
+            // 
+            this.lblCheckResult3.AutoSize = true;
+            this.lblCheckResult3.Location = new System.Drawing.Point(690, 340);
+            this.lblCheckResult3.Name = "lblCheckResult3";
+            this.lblCheckResult3.Size = new System.Drawing.Size(41, 13);
+            this.lblCheckResult3.TabIndex = 18;
+            this.lblCheckResult3.Text = "Check!";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCheckResult3);
+            this.Controls.Add(this.lblCheckResult2);
+            this.Controls.Add(this.lblCheckResult1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.CorrectButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.CorrectButton1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.Right3Button);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Right3Button;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button CorrectButton1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button CorrectButton;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblCheckResult1;
+        private System.Windows.Forms.Label lblCheckResult2;
+        private System.Windows.Forms.Label lblCheckResult3;
+    }
+}
